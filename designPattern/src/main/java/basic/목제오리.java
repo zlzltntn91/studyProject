1811a47627d0cfff0d7d15e325e3a49818ccdb50
zlzltntn91 <zlamstn91@gmail.com>
2021-02-08 23:52:09 +0900

    home designPattern

---
+++ designPattern/src/main/java/basic/목제오리.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class 목제오리 extends 오리 {
+  @Override
+  public void 모습 () {
+    System.out.println("나무로됨 황토색");
+  }
+
+  @Override
+  public void 꽥꽥 () {
+    // 소리못냄
+  }
+
+  @Override
+  public void 날다 () {
+    // 날지못함
+  }
+}
